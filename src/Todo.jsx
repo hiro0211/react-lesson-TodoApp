10b0382ec4082a9ff5650fe200f194cad8acfdf9
arimu <arimurahiroaki40@gmail.com>
2024-11-15 08:26:42 +0900

    骨組み部分は作成

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -5,47 +5,60 @@ export const Todo = () => {
   const [todoText, setTodoText] = useState("");
   const [incompleteTodos, setIncompleteTodos] = useState([""]);
   const [completeTodos, setCompleteTodos] = useState(["ううう", "えええ"]);
+  // const [status, setStatus] = useState("未着手", "作業中", "完了");
 
   const onChangeTodoText = (e) => setTodoText(e.target.value);
 
-  const onClickAdd =() => {
+  const filterOptions = [
+    { value: "all", label: "すべて" },
+    { value: "notStarted", label: "未着手" },
+    { value: "inProgress", label: "作業中" },
+    { value: "done", label: "完了" },
+  ];
+
+  // const onClickChangeStatus = (e) => setStatus(e.target.value);
+
+  const onClickAdd = () => {
     if (todoText === "") return;
     const newTodos = [...incompleteTodos, todoText];
     setIncompleteTodos(newTodos);
     setTodoText("");
-  }
-    
+  };
+
   const onCLickEdit = () => {
     const newTodos = [...incompleteTodos, todoText];
     setIncompleteTodos(newTodos);
     setTodoText("");
-  }
+  };
 
   const onClickDelete = (index) => {
     const newTodos = [...incompleteTodos];
     newTodos.splice(index, 1);
     setIncompleteTodos(newTodos);
-  }
+  };
 
   // 全てのTodoを全削除
-  const onClickDeleteAll = () =>{
+  const onClickDeleteAll = () => {
     const newTodos = [...incompleteTodos, ...completeTodos];
     newTodos.splice(0, newTodos.length);
     setIncompleteTodos(newTodos);
     setCompleteTodos(newTodos);
-  }
+  };
 
   return (
     <>
       <div>
-        <input onChange={onChangeTodoText} placeholder="Todoを入力" value={todoText}/>
+        <input
+          onChange={onChangeTodoText}
+          placeholder="Todoを入力"
+          value={todoText}
+        />
         <button onClick={onClickAdd}>追加</button>
-        <button onClick={()=> onClickDeleteAll()}>全削除</button>
+        <button onClick={() => onClickDeleteAll()}>全削除</button>
       </div>
       <div>
         <div>
-          <p>未完了のTodo</p>
-
+          <p>Todo</p>
         </div>
         <ol>
           {incompleteTodos.map((todo, index) => {
@@ -53,29 +66,20 @@ export const Todo = () => {
               <li key={todo}>
                 <div>
                   <p>{todo}</p>
-                  <button>完了</button> 
+                  {/* <button>完了</button> */}
                   <button onClick={() => onClickDelete(index)}>削除</button>
-                  <button onClick={onCLickEdit}>編集</button>
+                  <select>
+                    {filterOptions.map(({ value, label }) => (
+                      <option value={value}>{label}</option>
+                    ))}
+                  </select>
                 </div>
               </li>
             );
           })}
         </ol>
       </div>
-      <div>
-        <p>完了のTodo</p>
-
-        <ol>
-          {completeTodos.map((todo) => {
-            return (
-              <li key={todo}>
-                <p>{todo}</p>
-                <button>戻す</button>
-              </li>
-            );
-          })}
-        </ol>
-      </div>
+      <div></div>
     </>
   );
 };
